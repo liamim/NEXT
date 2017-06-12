91e3aa748b28003ee4b5efcbe42628a6c5eb36fe
Liam Marshall <limarshall@wisc.edu>
2017-06-12 12:36:08 -0500

    Add a workaround for boto/boto#2207

--- next/assistant/s3.py
+++ next/assistant/s3.py
@@ -1,3 +1,4 @@
+import boto.s3 as s3
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 import next.utils as utils
@@ -18,7 +19,16 @@ def get_bucket(AWS_BUCKET_NAME, AWS_ID, AWS_KEY):
     Creates a bucket for an S3 account
     """
     conn = S3Connection(AWS_ID, AWS_KEY)
+
+    # Workaround for boto/boto#2207
     bucket = conn.get_bucket(AWS_BUCKET_NAME, validate=False)
+    bucket_location = bucket.get_location()
+    if bucket_location:
+        connection = s3.connect_to_region(bucket_location,
+            aws_access_key_id=AWS_ID,
+            aws_secret_access_key=AWS_KEY)
+        bucket = conn.get_bucket(AWS_BUCKET_NAME, validate=False)
+
     return bucket
 
 def upload(filename, file_object, bucket):
