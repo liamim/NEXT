5e30abdfe38e9e48465e159f85ea6b9a1f5a738a
Liam Marshall <limarshall@wisc.edu>
2018-06-29 16:46:50 -0500

    targets prep: include grid location

--- local/cat_dog_prep.py
+++ local/cat_dog_prep.py
@@ -5,11 +5,13 @@ import yaml
 G = nx.grid_2d_graph(21, 21)
 filepaths = []
 neighbors = []
+locations = []
 for i, p in enumerate(Path('.').glob('cat_dog/*.png')):
     x, y = map(int, p.stem.split('_'))
     n = (x+10, y+10)
-    neighbors.append(G.neighbors(n))
-    G[n]['i'] = i
+    neighbors.append(list(G.neighbors(n)))
+    G.nodes[n]['i'] = i
+    locations.append((x, y))
     filepaths.append(p)
 
 targets = [
@@ -18,9 +20,10 @@ targets = [
      'alt_description': '',
      'alt_type': '',
      # 'neighbors_': ns,
-     'neighbors': [G[n]['i'] for n in ns],
+     'location': loc,
+     'neighbors': [G.nodes[n]['i'] for n in ns],
     }
-    for p, ns in zip(filepaths, neighbors)
+    for p, ns, loc in zip(filepaths, neighbors, locations)
 ]
 
 print(yaml.safe_dump(targets))
