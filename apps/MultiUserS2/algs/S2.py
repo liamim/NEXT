6da91d221c518f41f7e4be58af3e1734e2e87224
Liam Marshall <limarshall@wisc.edu>
2018-07-24 12:06:22 -0500

    start working on the master

--- apps/MultiUserS2/algs/S2.py
+++ apps/MultiUserS2/algs/S2.py
@@ -6,11 +6,32 @@ from networkx.readwrite import json_graph
 import itertools
 from collections import deque
 from next.utils import debug_print, profile_each_line
+import operator
+from pprint import pprint
 
 class Status:
     NOT_ASSIGNED = 'not_assigned'
     WAITING      = 'waiting'
     COMPLETED    = 'completed'
+
+class Master:
+    def __init__(self):
+        pass
+
+    def init_jobs(self):
+        pass
+
+    def get_vertex_for(self, user):
+        if jobs == []:
+            # handle empty job list case
+            pass
+
+        opt_job = max([{'job': job, 'priority': priority(job, user, state)} for job in jobs],
+                      key=operator.itemgetter('priority'))
+
+        if opt_job['priority'] == -np.inf:
+            # handle no assignable jobs case
+            pass
 
 class MyAlg:
     def initExp(self, butler, n):
